3bf3d9012290a0afe0f8edc2c1593c4423f84369
Jan Trejbal <jan@trejbal.land>
2024-11-28 00:52:18 +0100

    Use trailing slash for pages

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type {NextConfig} from "next";
 const nextConfig: NextConfig = {
     output: "export",  // <=== enables static exports
     reactStrictMode: true,
+    transpilePackages: ['lucide-react'],
+    trailingSlash: true,
 };
 
 export default nextConfig;
