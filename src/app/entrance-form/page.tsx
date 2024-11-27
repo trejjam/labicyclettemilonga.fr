032fe51b1cc1ffaf338df00051b6f566d4b23202
Jan Trejbal <jan@trejbal.land>
2024-11-27 23:53:39 +0100

    Add EntranceForm redirect

---
+++ src/app/entrance-form/page.tsx
@@ -0,0 +1,6 @@
+'use client'
+import {redirect} from "next/navigation";
+
+export default function EntranceForm() {
+    redirect('https://docs.google.com/forms/d/e/1FAIpQLScXsf3q7sqpU0TpIgkAtoTlUHDy50IuU5tb6dSLUUcXsmUGBQ/viewform?usp=sf_link');
+}
