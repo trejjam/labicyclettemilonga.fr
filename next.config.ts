6bd69a7f5537d44e01cd5a3a3fdc270b5a219a5e
Jan Trejbal <jan@trejbal.land>
2024-11-28 00:59:23 +0100

    Form redirect (#20)
    * Update dependencies
    
    * Update tsconfig.json
    
    * Update template
    
    * Update template
    
    * Add EntranceForm redirect
    
    * Fix FeatureBar
    
    * Use trailing slash for pages
    
    * Add shadcn
    
    * Use shadcn NavBar
    
    * Bump marathon/page
    
    * Update Layout
    
    * Add lint action

---
+++ next.config.ts
@@ -0,0 +1,10 @@
+import type {NextConfig} from "next";
+
+const nextConfig: NextConfig = {
+    output: "export",  // <=== enables static exports
+    reactStrictMode: true,
+    transpilePackages: ['lucide-react'],
+    trailingSlash: true,
+};
+
+export default nextConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,38 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import type {Metadata} from "next";
+import localFont from "next/font/local";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const geistSans = localFont({
+    src: "./fonts/GeistVF.woff",
+    variable: "--font-geist-sans",
+    weight: "100 900",
+});
+const geistMono = localFont({
+    src: "./fonts/GeistMonoVF.woff",
+    variable: "--font-geist-mono",
+    weight: "100 900",
+});
 
 export const metadata: Metadata = {
-  title: "La Bicycletta Milonga",
-  description: "La milonga de Toulouse",
+    title: "La Bicycletta Milonga",
+    description: "La milonga de Toulouse",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+export default function RootLayout(
+    {
+        children,
+    }: Readonly<{
+        children: React.ReactNode;
+    }>
+) {
+    return (
+        <html lang="fr">
+        <body
+            className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+        {/*<NavBar />*/}
+        {children}
+        </body>
+        </html>
+    );
 }
