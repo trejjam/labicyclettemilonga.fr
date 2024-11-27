700769b6fb74cf690ce58eb0bf9b0a9e36c599f4
Jan Trejbal <jan@trejbal.land>
2024-11-27 22:47:06 +0100

    Update template

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,32 @@
+'use client'
+
+import {Button} from "theme-ui";
+import NoSSR from "@/components/common/NoSSR.tsx";
+import {useAcceptCookies} from "@/lib/hooks/useAcceptCookies.tsx";
+import dynamic from "next/dynamic";
+
+const FeatureBar = dynamic(() => import('@/components/common/FeatureBar.tsx'), {
+    ssr: false,
+})
+
 export default function Home() {
-  return (
-    <main className="flex">
-      Work in progress
-    </main>
-  );
+    const {acceptedCookies, onAcceptCookies} = useAcceptCookies()
+
+    return (
+        <>
+            <main className="flex">
+                Work in progress
+            </main>
+            <NoSSR>
+                <FeatureBar
+                    title="This site uses cookies to improve your experience. By clicking, you agree to our Privacy Policy."
+                    hide={acceptedCookies}
+                    action={
+                        <Button onClick={() => onAcceptCookies()}>Accept cookies</Button>
+                    }
+                >
+                </FeatureBar>
+            </NoSSR>
+        </>
+    );
 }
---
+++ src/components/common/FeatureBar.tsx
@@ -0,0 +1,17 @@
+import {ReactNode} from "react";
+
+export default function FeatureBar(
+    {
+        title,
+        hide,
+        action,
+    }: {
+        title: string,
+        hide: boolean,
+        action: ReactNode,
+    }
+) {
+    return (
+        <></>
+    );
+}
