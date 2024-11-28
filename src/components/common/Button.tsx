ef91d0e2d7e295c8c766d7d7f378ecff2c5fb2f6
Jan Trejbal <jan@trejbal.land>
2024-11-28 01:03:11 +0100

    Add empty button component

---
+++ src/components/common/Button.tsx
@@ -0,0 +1,13 @@
+import {ReactNode} from "react";
+
+export function Button(
+    {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        onClick, children
+    }: {
+        onClick: () => void,
+        children: ReactNode
+    }
+) {
+    return null;
+}
