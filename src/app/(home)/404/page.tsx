a78608ca0d7e7d4897e46e5318a38989a9800845
Jan Trejbal <jan@trejbal.land>
2024-12-03 18:07:37 +0100

    Custom 404 (#28)
    * Update dependencies
    
    * Use custom 404

--- src/app/(home)/404/page.tsx
+++ src/app/(home)/404/page.tsx
@@ -1,6 +1,19 @@
-import GenericNotFoundPage from '@/app/[lang]/404/page.tsx';
+'use client';
+
 import { Params } from '@/app/[lang]/page.tsx';
+import { use } from 'react';
+import { defaultLocale } from '@/config/i18n.ts';
+import { useTranslation } from '@/hooks/use-translation.ts';
 
 export default function NotFoundPage({ params }: { params: Params }) {
-  return <GenericNotFoundPage params={params} />;
+  const lang = use(params).lang ?? defaultLocale;
+
+  const { t } = useTranslation({ lng: lang, ns: 'not-found' });
+
+  return (
+    <main>
+      <h2>{t('title')}</h2>
+      <p>Could not find requested resource</p>
+    </main>
+  );
 }
