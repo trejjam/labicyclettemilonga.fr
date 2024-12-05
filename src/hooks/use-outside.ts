adc4c93f71cad2740576be395458c4c997e933eb
Jan Trejbal <jan@trejbal.land>
2024-12-05 23:46:12 +0100

    Drop unused comments

--- src/hooks/use-outside.ts
+++ src/hooks/use-outside.ts
@@ -1,17 +1,11 @@
 import { RefObject, useEffect, useRef } from 'react';
 
-/**
- * Hook that alerts clicks outside of the passed ref
- */
 export default function useOutside(callback: {
   (): void;
 }): RefObject<HTMLElement | null> {
   const ref = useRef<HTMLElement>(null);
 
   useEffect(() => {
-    /**
-     * Alert if clicked on outside of element
-     */
     function handleClickOutside(event: MouseEvent) {
       if (ref.current && !ref.current.contains(event.target as HTMLElement)) {
         callback();
