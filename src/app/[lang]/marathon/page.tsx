4409524bd6f970a389b65af2cc2a9f979e7738b0
NicoMacondo <nicolas.arribat@gmail.com>
2024-12-23 11:19:57 +0100

    Update page.tsx

--- src/app/[lang]/marathon/page.tsx
+++ src/app/[lang]/marathon/page.tsx
@@ -246,6 +246,51 @@ export default function GenericMarathonPage({ params }: { params: Params }) {
           <div className='clear-both'></div>
         </div>
       </section>
+      
+      <section className='bg-slate-900'>
+        <div className='m-auto flex max-w-5xl flex-wrap justify-center px-12 py-12'>
+          <h1 className='my-8 text-center text-4xl font-extrabold capitalize text-gray-300 lg:text-6xl'>
+            {t('marathon.timetable')}
+          </h1>
+          <div className='w-0 basis-full'></div>
+            <Image
+            src='/img/marathon/2025/dj-carmen.jpg'
+            width={836}
+            height={836}
+            alt='TDJ Carmen Maria Hergos'
+            className='max-w-96 object-scale-down pb-2 pr-2 md:w-6/12 lg:w-5/12'
+            />
+            <p className='mt-4 text-center md:ml-12 md:mt-0 md:w-5/12 md:text-left lg:ml-12'>
+            {t('marathon.dj.carmen')}
+            </p>
+          <div className='clear-both'></div>
+         <div className='w-0 basis-full'></div>
+            <Image
+            src='/img/marathon/2025/dj-carmen.jpg'
+            width={836}
+            height={836}
+            alt='TDJ Carmen Maria Hergos'
+            className='max-w-96 object-scale-down pb-2 pr-2 md:w-6/12 lg:w-5/12'
+            />
+            <p className='mt-4 text-center md:ml-12 md:mt-0 md:w-5/12 md:text-left lg:ml-12'>
+            {t('marathon.dj.carmen')}
+            </p>
+          <div className='clear-both'></div>
+         <div className='w-0 basis-full'></div>
+            <Image
+            src='/img/marathon/2025/dj-carmen.jpg'
+            width={836}
+            height={836}
+            alt='TDJ Carmen Maria Hergos'
+            className='max-w-96 object-scale-down pb-2 pr-2 md:w-6/12 lg:w-5/12'
+            />
+            <p className='mt-4 text-center md:ml-12 md:mt-0 md:w-5/12 md:text-left lg:ml-12'>
+            {t('marathon.dj.carmen')}
+            </p>
+          <div className='clear-both'></div>
+        </div>
+      </section>
+      
       <div suppressHydrationWarning={true}>
         <DynamicVenueMap lang={lang} />
       </div>
