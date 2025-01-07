057c1bb7dacb96e5eb210e5e25dbb9587d0bbbca
Jan Trejbal <jan@trejbal.land>
2025-01-07 17:34:31 +0100

    Add signup form (#156)

---
+++ src/app/(home)/marathon/signup/page.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import GenericMarathonSignupPage from '@/app/[lang]/marathon/signup/page.tsx';
+
+export default function MarathonSignupPage() {
+  return <GenericMarathonSignupPage />;
+}
