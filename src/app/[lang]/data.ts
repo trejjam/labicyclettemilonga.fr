aa0685763d9e21490fb4d23a282cfb88127b4cf0
Jan Trejbal <jan@trejbal.land>
2025-06-25 13:04:33 +0200

    Add milonga 2025 (#336)
    * Add new dates
    
    * Tweak alt texts

--- src/app/[lang]/data.ts
+++ src/app/[lang]/data.ts
@@ -23,4 +23,36 @@ export const milongaDates = [
     starts: new Date(2025, 3, 19, 20, 30, 0),
     ends: new Date(2025, 3, 20, 2, 30, 0),
   },
+  {
+    starts: new Date(2025, 8, 20, 19, 0, 0),
+    ends: new Date(2025, 8, 21, 1, 0, 0),
+  },
+  {
+    starts: new Date(2025, 9, 4, 19, 0, 0),
+    ends: new Date(2025, 9, 5, 1, 0, 0),
+  },
+  {
+    starts: new Date(2025, 10, 8, 19, 0, 0),
+    ends: new Date(2025, 10, 9, 1, 0, 0),
+  },
+  {
+    starts: new Date(2025, 11, 13, 19, 0, 0),
+    ends: new Date(2025, 11, 14, 1, 0, 0),
+  },
+  {
+    starts: new Date(2026, 0, 17, 19, 0, 0),
+    ends: new Date(2026, 0, 18, 1, 0, 0),
+  },
+  {
+    starts: new Date(2026, 1, 7, 19, 0, 0),
+    ends: new Date(2026, 1, 8, 1, 0, 0),
+  },
+  {
+    starts: new Date(2026, 2, 14, 19, 0, 0),
+    ends: new Date(2026, 2, 15, 1, 0, 0),
+  },
+  {
+    starts: new Date(2026, 3, 18, 19, 0, 0),
+    ends: new Date(2026, 3, 19, 1, 0, 0),
+  },
 ];
