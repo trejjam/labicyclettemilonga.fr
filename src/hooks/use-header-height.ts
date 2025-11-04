45b8f6dacc3e215668677fe93f5e2dff04e3fd1f
Jan Trejbal <jan@trejbal.land>
2025-11-04 14:01:03 +0100

    Fix useEffect issues

--- src/hooks/use-header-height.ts
+++ src/hooks/use-header-height.ts
@@ -1,14 +1,16 @@
-import { useEffect, useState } from 'react';
+import { useLayoutEffect, useState } from 'react';
 
 export const useHeaderHeight = (initialHeight: number = 0) => {
   const [headHeight, setHeadHeight] = useState(initialHeight);
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     const header = document.getElementById('header');
 
     if (header === null) return;
 
-    setHeadHeight(header.getBoundingClientRect().height);
+    queueMicrotask(() => {
+      setHeadHeight(header.getBoundingClientRect().height);
+    });
 
     const resizeObserver = new ResizeObserver(() => {
       setHeadHeight(header.getBoundingClientRect().height);
